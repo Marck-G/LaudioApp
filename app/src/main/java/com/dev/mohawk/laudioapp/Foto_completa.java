c5ba14e93bf1eddb5a2e385096cd2d5deebedd7f
Ibon <ZzibonxX@gmail.com>
2018-12-13 10:16:51 +0100

    Subido la actividad foto_completa

---
+++ app/src/main/java/com/dev/mohawk/laudioapp/Foto_completa.java
@@ -0,0 +1,15 @@
+package com.dev.mohawk.laudioapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dev.mohawk.laudioapp.R;
+
+public class Foto_completa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foto_completa);
+    }
+}
