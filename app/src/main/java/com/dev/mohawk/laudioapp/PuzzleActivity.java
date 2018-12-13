687e279bc28f60c88baa1f0711b7b8829ec15466
Marck <marckcarrion@gmail.com>
2018-12-13 13:37:59 +0100

    Añadido un delay al terminar el puzle.

--- app/src/main/java/com/dev/mohawk/laudioapp/PuzzleActivity.java
+++ app/src/main/java/com/dev/mohawk/laudioapp/PuzzleActivity.java
@@ -15,6 +15,7 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.media.ExifInterface;
 import android.net.Uri;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -287,10 +288,15 @@ public class PuzzleActivity extends AppCompatActivity {
 
         return ret;
     }
-
+//    TODO: cerrar el la actividad, cambiar para que no se cierre
     public void checkGameOver() {
         if (isGameOver()) {
-            finish();
+            new Handler().postDelayed( new Runnable() {
+                @Override
+                public void run() {
+                    finish();
+                }
+            }, 3000 );
         }
     }
 
