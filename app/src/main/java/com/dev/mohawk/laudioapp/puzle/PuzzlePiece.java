4cdee1b1e4b4d3fa8d6a9156cdee69a3b6a84bf1
Marck <marckcarrion@gmail.com>
2018-12-13 13:13:34 +0100

    Añadido el puzle con errores

---
+++ app/src/main/java/com/dev/mohawk/laudioapp/puzle/PuzzlePiece.java
@@ -0,0 +1,15 @@
+package com.dev.mohawk.laudioapp.puzle;
+
+import android.content.Context;
+
+public class PuzzlePiece extends android.support.v7.widget.AppCompatImageView {
+    public int xCoord;
+    public int yCoord;
+    public int pieceWidth;
+    public int pieceHeight;
+    public boolean canMove = true;
+
+    public PuzzlePiece(Context context) {
+        super(context);
+    }
+}
