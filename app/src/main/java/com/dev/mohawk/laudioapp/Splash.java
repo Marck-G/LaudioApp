427114925b416c1a7851f659a476d47b677bf9f7
Ibon <ZzibonxX@gmail.com>
2018-12-13 13:07:06 +0100

    Arreglado el problema del icono

--- app/src/main/java/com/dev/mohawk/laudioapp/Splash.java
+++ app/src/main/java/com/dev/mohawk/laudioapp/Splash.java
@@ -1,17 +1,18 @@
 package com.dev.mohawk.laudioapp;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import com.dev.mohawk.laudioapp.MainActivity;
 import com.dev.mohawk.laudioapp.R;
 
 public class Splash extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
@@ -19,7 +20,7 @@ public class Splash extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         new Handler().postDelayed(new Runnable() {
-            @Override
+
             public void run() {
                 Intent intento=new Intent(Splash.this,MainActivity.class);
                 startActivity(intento);
