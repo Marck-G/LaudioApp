c1fcb4bbeecb984567a2712aaaf267cfe8b3529d
Marck C. Guzmán <marck.guzman.dev@gmail.com>
2019-01-29 17:25:25 +0100

    Separada la lectura del horario en otro hilo
    Signed-off-by: Marck C. Guzmán <marck.guzman.dev@gmail.com>

--- app/src/main/java/com/dev/mohawk/laudioapp/HorarioActivity.java
+++ app/src/main/java/com/dev/mohawk/laudioapp/HorarioActivity.java
@@ -1,9 +1,11 @@
 package com.dev.mohawk.laudioapp;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.dev.mohawk.laudioapp.horarios.HorariosListAdapter;
 import com.marck.renfeApi.contentSelector.Selector;
@@ -22,15 +24,25 @@ public class HorarioActivity extends AppCompatActivity {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_horario );
         try {
-            Selector s = Selector.build().setDocument( new BufferedReader( new FileReader( getFileStreamPath( LLODIO_ST_H_FILE ) ) ) );
-            String[][] data = s.getTabla();
-            String info = String.format( getString( R.string.h_last_update ), s.getDate() );
-            HorariosListAdapter ad = new HorariosListAdapter( this, R.id.h_lista, data );
-            ( ( ListView )findViewById( R.id.h_lista ) ).setAdapter( ad );
+           final Selector s = Selector.build().setDocument( new BufferedReader( new FileReader( getFileStreamPath( LLODIO_ST_H_FILE ) ) ) );
+
             ( ( TextView ) findViewById( R.id.h_info ) ).setText( s.getOrigen() + " - " + s.getDestino() );
-            ( ( TextView ) findViewById( R.id.h_date ) ).setText( info );
-        } catch ( Selector.EmptyDocumentException e ) {
-            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "Leyendo los horarios...", Toast.LENGTH_LONG ).show();
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    String[][] data = new String[0][];
+                    try {
+                        data = s.getTabla();
+                    } catch (Selector.EmptyDocumentException e) {
+                        e.printStackTrace();
+                    }
+                    String info = String.format( getString( R.string.h_last_update ), s.getDate() );
+                    HorariosListAdapter ad = new HorariosListAdapter( HorarioActivity.this, R.id.h_lista, data );
+                    ( ( ListView )findViewById( R.id.h_lista ) ).setAdapter( ad );
+                    ( ( TextView ) findViewById( R.id.h_date ) ).setText( info );
+                }
+            }, 30 );
         } catch ( FileNotFoundException e ) {
             e.printStackTrace();
         } catch ( IOException e ) {
