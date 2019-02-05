08f01c510094d7ec4daadd46471784421dd8c61f
Ibon <ZzibonxX@gmail.com>
2019-02-05 10:33:42 +0100

    TrenGeltokia2

--- app/src/main/java/com/dev/mohawk/laudioapp/Foto_completa.java
+++ app/src/main/java/com/dev/mohawk/laudioapp/Foto_completa.java
@@ -17,8 +17,7 @@ public class Foto_completa extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        Intent i = new Intent( this, NavegacionActivity.class );
-        i.putExtra( NavegacionActivity.DESTINO, Places.getId( Places.ELIZA ));
+        Intent i = new Intent( this, TrenGeltokia1.class );
         startActivity( i );
         finish();
     }
