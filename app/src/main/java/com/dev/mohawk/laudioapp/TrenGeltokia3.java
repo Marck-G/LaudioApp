5ecf6e57399be3118c8a642ec5dba3cabbe7acd1
Ibon <ZzibonxX@gmail.com>
2019-02-07 09:38:46 +0100

    Trengeltokia3

---
+++ app/src/main/java/com/dev/mohawk/laudioapp/TrenGeltokia3.java
@@ -0,0 +1,13 @@
+package com.dev.mohawk.laudioapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TrenGeltokia3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tren_geltokia3);
+    }
+}
