d9bc95bddc0f6895b6edb972a5f4b86ea4362f08
Ibon <ZzibonxX@gmail.com>
2019-02-08 09:13:39 +0100

    TrenGeltokia3.class

--- app/src/main/java/com/dev/mohawk/laudioapp/TrenGeltokia3.java
+++ app/src/main/java/com/dev/mohawk/laudioapp/TrenGeltokia3.java
@@ -1,13 +1,66 @@
 package com.dev.mohawk.laudioapp;
 
+import android.annotation.SuppressLint;
+import android.os.Build;
+import android.os.Handler;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class TrenGeltokia3 extends AppCompatActivity {
 
-    @Override
+
+    private ImageView aukera1,aukera2,aukera3,persona,jarraitu;
+    private TextView txomin,continuar;
+
+    @SuppressLint("ClickableViewAccessibility")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tren_geltokia3);
+
+        aukera1 = findViewById(R.id.opcion1);
+        aukera2 = findViewById(R.id.opcion2);
+        aukera3 = findViewById(R.id.opcion3);
+        persona = findViewById(R.id.persona);
+        txomin = findViewById(R.id.preguntaTren);
+        jarraitu = findViewById(R.id.jarraitu);
+        continuar = findViewById(R.id.jarraituText);
+
     }
+    public void elegirBien(View view){
+
+        aukera1.setImageResource(R.drawable.ic_btncorrect);
+        txomin.setText(R.string.TrenGeltokia3_zuzena);
+        persona.setImageResource(R.drawable.txomin);
+        new Handler().postDelayed(new Runnable() {
+
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            public void run() {
+                jarraitu.setVisibility(View.VISIBLE);
+                continuar.setVisibility(View.VISIBLE);
+                aukera1.setImageResource(R.drawable.ic_btn22);
+            }
+        },3000);
+    }
+
+    public void elegirMal(View view){
+
+        aukera1.setImageResource(R.drawable.ic_btnwrong);
+        txomin.setText(R.string.TrenGeltokia3_okerra);
+        persona.setImageResource(R.drawable.txomin);
+        new Handler().postDelayed(new Runnable() {
+
+
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            public void run() {
+                aukera1.setImageResource(R.drawable.ic_btn22);
+            }
+        },4000);
+    }
+
+
 }
