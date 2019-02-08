0c7625092ceb72e743ce95832b0b0448950bfa3c
Ibon <ZzibonxX@gmail.com>
2019-02-08 09:33:27 +0100

    TRENGELTOKIA 4.class

---
+++ app/src/main/java/com/dev/mohawk/laudioapp/TrenGeltokia4.java
@@ -0,0 +1,18 @@
+package com.dev.mohawk.laudioapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class TrenGeltokia4 extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tren_geltokia4);
+    }
+
+    public void continuar4(View view){
+
+
+    }
+}
