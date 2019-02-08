4b6f741fb484754cee078aa9dc7bbc439e41f99b
Marck C. Guzmán <marck.guzman.dev@gmail.com>
2019-02-08 21:13:30 +0100

    Modificada la actividad Zeramika3
    Signed-off-by: Marck C. Guzmán <marck.guzman.dev@gmail.com>

--- app/src/main/java/com/dev/mohawk/laudioapp/Zeramika3Activity.java
+++ app/src/main/java/com/dev/mohawk/laudioapp/Zeramika3Activity.java
@@ -2,12 +2,22 @@ package com.dev.mohawk.laudioapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class Zeramika3Activity extends AppCompatActivity {
+
+    private TextView txomin;
+    private String[] texts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zeramika3);
+//        flag para fullscreen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        txomin = findViewById( R.id.txomin13 );
+        texts = getString( R.string.txomin13 ).split( ";" );
+        txomin.setText( texts[0] );
     }
 }
